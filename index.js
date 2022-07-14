a1f2e081ff545100312650fc3025e1e7cb3f91a5
Erik <89684452+Arete-sqrt81@users.noreply.github.com>
2022-07-15 00:48:59 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "cruddatabase",
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/get", (req, res) => {
+  const sqlSelect = "SELECT * FROM movie_reviews";
+  db.query(sqlSelect, (err, result) => {
+    res.send(result);
+  });
+});
+app.post("/api/insert", (req, res) => {
+  const movie_name = req.body.movie_name;
+  const movie_review = req.body.movie_review;
+
+  const sqlInsert =
+    "INSERT INTO movie_reviews (movie_name, movie_review) VALUES (?,?)";
+  db.query(sqlInsert, [movie_name, movie_review], (err, result) => {
+    console.log(err);
+  });
+});
+
+app.delete("/api/delete/:movie_name", (req, res) => {
+  const name = req.params.movie_name;
+  const sqlDelete = "DELETE FROM movie_reviews WHERE movie_name = ?";
+  db.query(sqlDelete, name, (err, results) => {
+    if (err) console.log(err);
+  });
+});
+
+app.put("/api/update", (req, res) => {
+  const name = req.body.movie_name;
+  const review = req.body.movie_review;
+  const sqlUpdate =
+    "UPDATE movie_reviews SET movie_review = ? WHERE movie_name = ? ";
+  db.query(sqlUpdate, [review, name], (err, results) => {
+    if (err) console.log(err);
+  });
+});
+
+app.listen(3001, () => {
+  console.log("running on port 3001");
+});
